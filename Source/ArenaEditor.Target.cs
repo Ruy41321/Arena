9b8c8a3b535d83341d876ee5e1924e4a9ff1d6e6
Luigi <luigipennisi14@gmail.com>
2026-02-06 11:42:56 +0100

    converted engine version to 5.7.2

--- Source/ArenaEditor.Target.cs
+++ Source/ArenaEditor.Target.cs
@@ -5,10 +5,20 @@ using System.Collections.Generic;
 
 public class ArenaEditorTarget : TargetRules
 {
-	public ArenaEditorTarget(TargetInfo Target) : base(Target)
+    public ArenaEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
+		
+		// 1. Usa la V6 per UE 5.7
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+
+		// 2. IMPORTANTE: Rimuovi o commenta la riga "Unique"
+		// BuildEnvironment = TargetBuildEnvironment.Unique; 
+
+		// 3. Forza il superamento del controllo sugli identificatori non definiti
+		// Questo permette di usare l'ambiente condiviso senza l'errore precedente
+		bOverrideBuildEnvironment = true;
 
 		ExtraModuleNames.AddRange( new string[] { "Arena" } );
 	}
